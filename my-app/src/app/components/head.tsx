6c74d15e3fa482c9fe4ede5f632fdbdbf34cbbe3
SarfrazAziz786 <162900723+SarfrazAziz786@users.noreply.github.com>
2024-12-07 20:12:09 +0500

    ui-ux-figma

---
+++ my-app/src/app/components/head.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <div>
+        <div className='w-[1440px] h-[48px] bg-black font-Ponnala text-white text-center'>
+        Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/navbar'
+import Header from './components/head'
+
+
+
+const Homepage = () => {
+  return (
+    <div>
+      <Header/>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Homepage
